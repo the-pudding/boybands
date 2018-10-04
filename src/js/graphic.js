bdfa0e8860cc69edced89a0f7ba6d40c6775d988
Amber Thomas <ProQuestionAsker@gmail.com>
2018-10-04 10:28:23 -0700

    Test run of lottie

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,9 +1,37 @@
 /* global d3 */
+import lottie from 'lottie-web'
+
+let animation = null
+let container = d3.select('#bodymovin')
+let container2 = document.getElementById('bodymovin')
 
 function resize() {}
 
+function loadAnimation(){
+	d3.loadData('assets/animation/data.json',
+		(err, response) => {
+			if (err) console.log(err)
+			else{
+				animation = response[0]
+				setupAnimation()
+			}
+		})
+}
+
+function setupAnimation(){
+	const animData = {
+		container: container2,
+		autoplay: true,
+		loop: true,
+		path: 'assets/animation/data.json'
+	}
+
+	let dance = lottie.loadAnimation(animData)
+
+}
+
 function init() {
-	console.log('Make something awesome!');
+	loadAnimation()
 }
 
 export default { init, resize };
