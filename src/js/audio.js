44bc562547c5492f608e786f4cad11e1b2291528
russellgoldenberg <russellgoldenberg@gmail.com>
2018-10-12 14:00:53 -0400

    basic audio

--- src/js/audio.js
+++ src/js/audio.js
@@ -1,2 +1,55 @@
-function init() {}
-export default { init };
+import { Howl, Howler } from 'howler';
+
+const bands = {};
+
+function slugify(str) {
+	return str.toLowerCase().replace(/[^\w]/g, '_');
+}
+
+function play(band) {
+	const name = slugify(band);
+	const audio = bands[name];
+	if (audio) {
+		// todo: if previous song playing, fade it out / stop
+		// play new one
+		// audio.play();
+	} else {
+		// todo
+	}
+}
+
+function load(filenames) {
+	let i = 0;
+	const path = 'assets/audio';
+	const loadNext = () => {
+		const f = filenames[i];
+
+		const t = new Howl({
+			src: `${path}/${f}.mp3`,
+			onload: () => {
+				bands[f] = t;
+				advance();
+			},
+			onend: () => {
+				// todo
+			},
+			onloaderror: advance
+			// onfade: () => {
+			// 	tracks[f].stop();
+			// }
+		});
+	};
+
+	const advance = err => {
+		i += 1;
+		if (i < filenames.length) loadNext();
+	};
+
+	loadNext();
+}
+
+function init(data) {
+	const filenames = data.map(d => slugify(d.band));
+	load(filenames);
+}
+export default { init, play };
--- src/js/graphic-history.js
+++ src/js/graphic-history.js
@@ -1,29 +1,25 @@
-import animation from './animation';
-import audio from './audio';
+import Animation from './animation';
+import Audio from './audio';
 
 const $section = d3.select('#history');
 const $figure = $section.select('.history__figure');
 const $boys = $figure.select('.figure__boys');
-const $bandInfo = $section.select('.figure__info')
-const $songYear =  $bandInfo.select('.figure__info--year')
-const $songName = $bandInfo.select('.figure__info--song')
-const $bandName = $bandInfo.select('.figure__info--band')
+const $bandInfo = $section.select('.figure__info');
+const $songYear = $bandInfo.select('.info__year');
+const $songName = $bandInfo.select('.info__song');
+const $bandName = $bandInfo.select('info__band');
 let $boy = null;
 
 let bandData = [];
 let currentBandIndex = -1;
-
-
 
 const layers = ['hair'];
 
 function updateAppearance({ boys }) {
 	boys.forEach((b, index) => {
 		const $b = $boy.filter((d, i) => i === index);
-		const name = b[index]
-		console.log({b})
 		// Update name
-		$b.select('.boy__name').text(b.name)
+		$b.select('.boy__name').text(b.name);
 		layers.forEach(l => {
 			// $b.selectAll(`.${l}`).st('opacity', 0);
 			// $b.select(`.${l}--${b[l]}`)
@@ -35,12 +31,12 @@ function updateAppearance({ boys }) {
 	});
 }
 
-function updateInfo({band, highest_pos_date, highest_song}){
-	const parseTime = d3.timeParse('%Y-%m-%d')
-	const getYear = d3.timeFormat('%Y')
-	$songYear.text(getYear(parseTime(highest_pos_date)))
-	$songName.text(highest_song)
-	$bandName.text(band)
+function updateInfo({ band, highest_pos_date, highest_song }) {
+	const parseTime = d3.timeParse('%Y-%m-%d');
+	const getYear = d3.timeFormat('%Y');
+	$songYear.text(getYear(parseTime(highest_pos_date)));
+	$songName.text(highest_song);
+	$bandName.text(band);
 }
 
 function swapBoys(dir) {
@@ -58,7 +54,8 @@ function swapBoys(dir) {
 
 	// change layer style
 	updateAppearance(data);
-	updateInfo(data)
+	updateInfo(data);
+	Audio.play(data.band);
 }
 
 function setupBoys() {
@@ -74,19 +71,22 @@ function setupBoys() {
 	const boyName = $boy
 		.append('text')
 		.attr('class', 'boy__name')
-		.text("name")
+		.text('name');
 
-	animation.create($boy.nodes());
+	Animation.create($boy.nodes());
 }
 
-function resize(){}
+function resize() {}
 
 function init(data) {
+	Audio.init(data);
 	bandData = data;
 	setupBoys();
-	swapBoys(1);
 	$section.classed('is-selected', true);
 	$section.on('click', () => swapBoys(1));
+	setTimeout(() => {
+		swapBoys(1);
+	}, 1000);
 }
 
 export default { init, resize };
